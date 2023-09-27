3d1ff5662e4bbc5dcc8fe2713b4a9061da967fac
Priyanshu Kumari <126251414+Priyanshu-89@users.noreply.github.com>
2023-09-27 16:12:55 +0000

    String in js

---
+++ javascript/string.js
@@ -0,0 +1,34 @@
+const name= "Javascript"
+const gitRepo=343
+console.log(name + gitRepo);
+
+//use backticks
+
+console.log(`Hello ${name} my git repo is ${gitRepo}` )
+
+//using object to create string
+
+const userName = new String('Priyanshu')
+console.log(userName);
+
+console.log(userName[0]);
+
+console.log(userName.length);
+
+console.log(userName.indexOf('r'));
+
+console.log(userName.toUpperCase());
+
+console.log(userName.charAt(4));
+
+console.log(userName.toLowerCase());
+
+console.log(userName.substring(0, 4));
+
+console.log(userName.slice(0, 6));
+
+console.log(userName.replace('priyanshu', 'kumari'));
+
+console.log(userName.includes('anshu'));
+
+console.log(userName.includes('esha'));

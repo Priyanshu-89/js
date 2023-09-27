7ff59acae9612fe4066a79dbcb83bce1ab0fd371
Priyanshu Kumari <126251414+Priyanshu-89@users.noreply.github.com>
2023-09-27 16:44:34 +0000

    Numbers and Math in js

---
+++ javascript/numbers_math.js
@@ -0,0 +1,34 @@
+const account = 300
+console.log(typeof(account))
+
+const account_balance= new Number(4544)
+console.log(account_balance);
+
+console.log(account_balance.toString().length);
+
+console.log(account_balance.toFixed(3));
+
+const anotherAccount=24.65554
+console.log(anotherAccount.toPrecision(3));
+
+const balance =100000000
+console.log(balance.toLocaleString('en-IN'));
+
+
+//*************************************************************************/
+
+      //  Maths
+
+//*************************************************************************/
+
+console.log(Math);
+
+console.log(Math.abs(5));
+
+console.log(Math.round(4.6));
+
+console.log(Math.floor(4.3))
+
+console.log(Math.ceil(4.2));
+
+console.log(Math.floor(Math.random()*10 )+1);
